a7ec18c32823bc59f7d9af3c420dde5903156f1a
harbans <harbansl506@gmail.com>
2024-04-04 15:05:48 +0530

    css js challenge 1.0

--- app.js
+++ app.js
@@ -19,17 +19,18 @@ console.log(`the value is ${value} and the all chaiing numbers are ${arr} and th
 
 let storeArLength = [];
 
-for(i=1; i< 100; i++){
+for(i=1; i< 1000000; i++){
   let [arr, value] = longestCollatz(i);
   let arrLen = arr.length;
   storeArLength.push({arrLen:arrLen, value:i});
 
 }
 
-let sommm = storeArLength.filter(val =>  val.arrLen ==106)
-console.log("hhhhh", sommm);
+let sommm = storeArLength.reduce((acc,val) =>val["arrLen"] > acc["arrLen"]?val:acc);
 
-console.log(storeArLength);
+
+// console.log("All array>>>",storeArLength);
+console.log("hhhhh", sommm);
 
 //2.>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
 
@@ -64,4 +65,59 @@ for(i=0; i<20;i++){
   arr2.push(innerArr);
 }
 
-console.log(arr2);
+
+const str = "AGGTAB";
+const str2 = "GXTXAYB";
+const allMatch = []
+
+function getAllIndexes(arr, val) {
+    var indexes = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === val) {
+            indexes.push(i);
+        }
+    }
+    return indexes;
+}
+
+
+function getNewValue(str1, str2) {
+  let finalArr = [];
+  
+  let arr2 = str2.split("");
+
+  for (let i = 0; i < str1.length; i++) {
+    const arr1 = str1.substr(i).split("");
+    let targetIndex = -1;
+    arr1.forEach((el, index) => {
+        let arr2Copy = [...arr2]
+        const allPossibleIndex = getAllIndexes(arr2Copy, el)
+      for (let j = 0; j < allPossibleIndex.length; j++ ){
+          
+          if (allPossibleIndex[j] > -1 && allPossibleIndex[j] > targetIndex || allPossibleIndex[j] == 0 && targetIndex != allPossibleIndex[j] && targetIndex < 1) {
+              finalArr.push(el);
+             targetIndex = allPossibleIndex[j];
+             break;
+      }
+      }
+    });
+    
+    allMatch.push(finalArr.join(""))
+    targetIndex = 0; 
+    finalArr = []
+  }
+}
+
+getNewValue(str, str2);
+
+console.log(allMatch)
+
+let longestMatchStr = ""
+
+allMatch.forEach(targetStr => {
+    if (targetStr.length > longestMatchStr.length) {
+        longestMatchStr = targetStr
+    }
+})
+
+console.log({longestMatchStr})
